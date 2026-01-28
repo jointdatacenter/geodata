6ed04a2cdb83d1d8382d90d3702d0457830be6b9
belisards <adrianobf@gmail.com>
2026-01-28 11:46:04 -0300

    update portfolio

--- docs/countries_data.js
+++ docs/countries_data.js
@@ -3,59 +3,111 @@
 
 const countryCategories = {
   ongoing: [
+
     "BEN", // Benin
+
     "BDI", // Burundi
-    "CMR", // Cameroon
+
     "CIV", // Côte d'Ivoire
+
     "GMB", // Gambia
+
     "GTM", // Guatemala
+
     "GNB", // Guinea-Bissau
+
     "HND", // Honduras
+
     "LBY", // Libya
+
     "MWI", // Malawi
+
     "MYS", // Malaysia
+
     "MLI", // Mali
+
     "MRT", // Mauritania
+
     "MDA", // Moldova
+
     "MOZ", // Mozambique
+
     "NER", // Niger
+
     "PAK", // Pakistan
+
     "COG", // Republic of the Congo
-    "SSD", // South Sudan
-    "SDN", // Sudan
-    "TGO", // Togo
-    "UGA", // Uganda
-    "UKR", // Ukraine
-    "TZA", // Tanzania (United Republic of Tanzania)
+    
     "SEN", // Senegal
-    "ZWE"  // Zimbabwe
+    
+    "SSD", // South Sudan
+    
+    "SDN", // Sudan
+    
+    "SYR", // Syria
+    
+    "TZA", // Tanzania (United Republic of Tanzania)
+
+    "UGA", // Uganda
+
+    "UKR", // Ukraine
+
   ],
+
   completed: [
-    "CHL", // Chile
-    "KAZ", // Kazakhstan
-   // "KRI", // KRI is not an ISO3 code (Kurdistan Region of Iraq)
-    "KGZ", // Kyrgyzstan
-    "TKM"  // Turkmenistan
-  ],
-  both: [
+
     "AFG", // Afghanistan
-    "BGD", // Bangladesh
-    "BFA", // Burkina Faso
-    "CAF", // Central African Republic
-    "TCD", // Chad
-    "COL", // Colombia
-    "DJI", // Djibouti
-    "COD", // Democratic Republic of the Congo
-    "ECU", // Ecuador
-    "ETH", // Ethiopia
-    "IRQ", // Iraq
-    "KEN", // Kenya
-    "JOR", // Jordan
+
+    "CHL", // Chile
+
+    "KAZ", // Kazakhstan
+
+   // "KRI", // KRI is not an ISO3 code (Kurdistan Region of Iraq)
+
+    "KGZ", // Kyrgyzstan
+    
     "LBN", // Lebanon
+
+    "TKM"  // Turkmenistan
+
+  ],
+
+  both: [
+
+    "BGD", // Bangladesh
+
+    "BFA", // Burkina Faso
+    
+    "CMR", // Cameroon
+
+    "CAF", // Central African Republic
+
+    "TCD", // Chad
+
+    "COL", // Colombia
+
+    "DJI", // Djibouti
+
+    "COD", // Democratic Republic of the Congo
+
+    "ECU", // Ecuador
+
+    "ETH", // Ethiopia
+
+    "IRQ", // Iraq
+
+    "KEN", // Kenya
+
+    "JOR", // Jordan
+
     "PER", // Peru
+
     "SOM", // Somalia
+
     "YEM", // Yemen
+
     "TUR"  // Türkiye (Turkey)
+
   ]
 };
 
